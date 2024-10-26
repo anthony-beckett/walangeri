15906cad6f9e4d6dfc7059cd2fea83f8de1153ff
Anthony Beckett <becay013@mymail.unisa.edu.au>
2024-10-26 20:00:50 +1030

    change ip addr again

--- src/services/login.js
+++ src/services/login.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://192.168.20.58:3001/api/login'
+const baseUrl = 'http://192.168.20.65:3001/api/login'
 
 const login = async credentials => {
     const response = await axios.post(baseUrl, credentials)

23765f6e8a3aaab3f7af4755ffbb372b340c5c83
Anthony Beckett <becay013@mymail.unisa.edu.au>
2024-11-03 20:52:51 +1030

    Allow images in reports to be viewed

--- src/components/Report.jsx
+++ src/components/Report.jsx
@@ -1,10 +1,15 @@
-import { View, Text, Button, Alert } from 'react-native';
+import {View, Text, Button, Alert, TouchableOpacity, Modal} from 'react-native';
 import allReportsStyle from '../styles/allReportsStyle';
 import { useNavigate } from 'react-router-native'
 import reportService from '../services/reports'
+import {Image} from "expo-image";
+import {newReportStyles} from "../styles/newReportStyles";
+import React, {useState} from "react";
 
 const Report = ({ report, reports, setReports }) => {
     const navigate = useNavigate()
+    const [showFullScreen, setShowFullScreen] = useState(false);
+
 
     const handleEdit = () => {
         navigate(`/editreport/${report.id}`, { state: { report } })
@@ -41,12 +46,50 @@ const Report = ({ report, reports, setReports }) => {
     return (
         <View style={allReportsStyle.reportContainer}>
             <Text style={allReportsStyle.reportName}>{report.reportName}</Text>
+            {report.image && (
+                <TouchableOpacity onPress={() => setShowFullScreen(true)}>
+                    <Image source={{ uri: `data:image/jpeg;base64,${report.image}` }}
+                       contentFit={'contain'}
+                       style={{
+                           width: 100,
+                           height: 100,
+                       }}
+                    />
+                </TouchableOpacity>
+                )}
             <Text>Address/Lot: {report.addressLot}</Text>
             <Text>Job Type: {report.jobType}</Text>
             <Text>Urgency Level: {report.urgencyLevel}</Text>
             <Text>Notes: {report.notes}</Text>
             <Button title="Edit" onPress={handleEdit} />
             <Button title="Delete" onPress={handleDelete} color="red" />
+
+            <Modal
+                presentationStyle={"overFullScreen"}
+                visible={showFullScreen}>
+                <View style={newReportStyles.container}>
+                    <Image
+                        source={{ uri: `data:image/jpeg;base64,${report.image}` }}
+                        contentFit="contain"
+                        style={{flex: 1}}
+                    />
+                    <TouchableOpacity onPress={() => {
+                        setShowFullScreen(false);
+                    }}
+                                      style={{
+                                          position: 'absolute',
+                                          top: 10,
+                                          right: 10,
+                                          // Semi-transparent background
+                                          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                                          borderRadius: 50,
+                                          padding: 10,
+                                      }}
+                    >
+                        <Text>❌</Text>
+                    </TouchableOpacity>
+                </View>
+            </Modal>
         </View>
     );
 };
